311ed5ec8bdec1a25e261a221d90a67242745e8e
lucascprazeres <olucascostadp@gmail.com>
2020-08-03 09:38:29 -0300

    minor: setup global styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
     <div className="App">
       <h1>Hello, Next Level Week!</h1>
+      <GlobalStyle />
     </div>
   );
 }
